b6a22566ca34083998ccd7f733f8982aea945227
AD0791 <alexdisla1991@gmail.com>
2021-09-28 15:59:00 -0400

    automation of the Layering Table engaged

---
+++ src/core/core_layering.py
@@ -0,0 +1,45 @@
+from core_agyw import AgywPrev
+from dataclasses import dataclass
+from typing import TypeVar
+
+DF = TypeVar('pandas.core.frame.DataFrame')
+
+base = AgywPrev().data_dreams_valid
+
+@dataclass(frozen=True)
+class LayerServices:
+    """convey the proper services given for the time"""
+    base1014: DF = base[base.age_range=="10-14"]
+    base1519: DF = base[base.age_range=="15-19"]
+    base2024: DF = base[base.age_range=="20-24"]
+    # CURRICULUM
+    curriculum_1014: int = base1014[(base1014.curriculum=="yes")].id_patient.count()
+    curriculum_1519: int = base1519[(base1519.curriculum=="yes")].id_patient.count()
+    curriculum_2024: int = base2024[(base2024.curriculum=="yes")].id_patient.count()
+    # CONDOM
+    condom_1014: int = base1014[(base1014.condom=="yes")].id_patient.count()
+    condom_1519: int = base1519[(base1519.condom=="yes")].id_patient.count()
+    condom_2024: int = base2024[(base2024.condom=="yes")].id_patient.count()
+    # HTS
+    hts_1014: int = base1014[(base1014.hts=="yes")].id_patient.count()
+    hts_1519: int = base1519[(base1519.hts=="yes")].id_patient.count()
+    hts_2024: int = base2024[(base2024.hts=="yes")].id_patient.count()
+    # Post care Violence
+    post_violence_care_1014: int = base1014[(base1014.post_violence_care=="yes")].id_patient.count()
+    post_violence_care_1519: int = base1519[(base1519.post_violence_care=="yes")].id_patient.count()
+    post_violence_care_2024: int = base2024[(base2024.post_violence_care=="yes")].id_patient.count()
+    # socio eco approche
+    socioeco_app_1014: int = base1014[(base1014.socioeco_app=="yes")].id_patient.count()
+    socioeco_app_1519: int = base1519[(base1519.socioeco_app=="yes")].id_patient.count()
+    socioeco_app_2024: int = base2024[(base2024.socioeco_app=="yes")].id_patient.count()
+    # prep
+    prep_1014: int = base1014[(base1014.prep=="yes")].id_patient.count()
+    prep_1519: int = base1519[(base1519.prep=="yes")].id_patient.count()
+    prep_2024: int = base2024[(base2024.prep=="yes")].id_patient.count()
+    # primary 
+    ps_1014: int = base1014[(base1014.ps_1014=="primary")].id_patient.count()
+    ps_1519: int = base1519[(base1519.ps_1519=="primary")].id_patient.count()
+    ps_2024: int = base2024[(base2024.ps_2024=="primary")].id_patient.count()
+    
+
+
