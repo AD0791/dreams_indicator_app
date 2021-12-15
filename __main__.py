2c0db8b55802ff2b6b8ec7e62d63e069a612be69
AD0791 <alexdisla1991@gmail.com>
2021-12-15 14:27:42 -0500

    ok

--- __main__.py
+++ __main__.py
@@ -2,5 +2,5 @@ import sys
 from streamlit import cli as stcli
 
 if __name__ == '__main__':
-    sys.argv = ["streamlit", "run", "./app/v22/dreams_indicator.py"]
+    sys.argv = ["streamlit", "run", "./app/dreams_data_application.py"]
     sys.exit(stcli.main())
