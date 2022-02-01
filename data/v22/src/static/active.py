0998fd59679d168f7e835ef3298c34d3a5ba30e2
AD0791 <alexdisla1991@gmail.com>
2022-02-01 10:27:46 -0500

    therapie

--- data/v22/src/static/active.py
+++ data/v22/src/static/active.py
@@ -21,7 +21,7 @@ class Set_date(Enum):
     master_start = "2017-10-01"
     master_end = "2022-01-31"
     period_start = "2021-10-01"
-    period_end = "2022-01-31"
+    period_end = "2022-02-01"
 
 
 # get the engine to connect and fetch
