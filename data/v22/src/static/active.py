b2494f53ff74de833116588249cecf01e42cc632
AD0791 <alexdisla1991@gmail.com>
2022-02-04 15:46:11 -0500

    resultat

--- data/v22/src/static/active.py
+++ data/v22/src/static/active.py
@@ -19,9 +19,9 @@ DBNAME = config('DBCaris')
 
 class Set_date(Enum):
     master_start = "2017-10-01"
-    master_end = "2022-01-31"
+    master_end = "2022-02-04"
     period_start = "2021-10-01"
-    period_end = "2021-12-31"
+    period_end = "2022-02-04"
 
 
 # get the engine to connect and fetch
