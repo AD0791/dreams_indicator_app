a96277015c89e6d60634c318e04ca047c66eabba
AD0791 <alexdisla1991@gmail.com>
2022-02-10 10:47:34 -0500

    results testing

--- data/v22/src/static/active.py
+++ data/v22/src/static/active.py
@@ -21,7 +21,7 @@ class Set_date(Enum):
     master_start = "2017-10-01"
     master_end = "2022-02-08"
     period_start = "2021-10-01"
-    period_end = "2022-02-08"
+    period_end = "2022-02-10"
 
 
 # get the engine to connect and fetch
