72891c68841e97a0c9768dc34c61db614e43faed
AD0791 <alexdisla1991@gmail.com>
2022-02-15 12:00:55 -0500

    End of the verification

--- data/v22/src/static/active.py
+++ data/v22/src/static/active.py
@@ -19,9 +19,9 @@ DBNAME = config('DBCaris')
 
 class Set_date(Enum):
     master_start = "2017-10-01"
-    master_end = "2022-02-14"
+    master_end = "2022-02-15"
     period_start = "2021-10-01"
-    period_end = "2022-02-14"
+    period_end = "2022-02-15"
 
 
 # get the engine to connect and fetch
