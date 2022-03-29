4dddd4dc160036f6d98a16766c69725933afa2e3
AD0791 <alexdisla1991@gmail.com>
2022-03-29 18:53:14 -0400

    2nd definition

--- OVC_SERV/ofunc.py
+++ OVC_SERV/ofunc.py
@@ -1,16 +1,15 @@
 from pandas import NaT
-
 def curriculum_detailed(pres):
-    if pres >= 14:
+    if pres >= 17:
         return "yes"
-    elif 1 <= pres <= 13:
+    elif 1 <= pres <= 16:
         return "has_started"
     else:
         return "no"
 
 
 def curriculum(pres):
-    return "yes" if pres >= 14 else "no"
+    return "yes" if pres >= 17 else "no"
 
 
 def parenting_detailed(pres):
@@ -27,19 +26,27 @@ def parenting(pres):
 
 
 def condom(df):
-    return "yes" if (df.has_comdom_topic == "yes" or df.number_of_condoms_sensibilize > 0 or df.number_condoms_reception_in_the_interval > 0 ) else "no" # or df.number_condoms_sensibilization_date_in_the_interval > 0
+    return "yes" if (df.has_comdom_topic == "yes" or df.number_of_condoms_sensibilize > 0 or df.number_condoms_reception_in_the_interval > 0 or df.number_condoms_sensibilization_date_in_the_interval > 0) else "no"
 
 
 def hts(hd):
     return "yes" if hd > 0 else "no"
 
 
+def vbg(vbg):
+    return "yes" if vbg > 0 else "no"
+
+
+def gyneco(gyneco):
+    return "yes" if gyneco > 0 else "no"
+
+
 def prep(pd):
     return "yes" if pd > 0 else "no"
 
+
 def contraceptive(cd):
     return "yes" if cd > 0 else "no"
-
 
 
 def postcare(df):
@@ -61,36 +68,34 @@ def prim_1519(df):
 def prim_2024(df):
     return "primary" if (df.age_range == "20-24" and df.curriculum == "yes" and df.condom == "yes") else "no"
 
-
 def sec_1014(df):
-    return "secondary" if (df.age_range == "10-14" and ((df.condom == "yes") | (df.hts == "yes") | (df.post_violence_care == "yes") | (df.socioeco_app == "yes") | (df.prep == "yes"))) else "no"
+    return "secondary" if (df.age_range == "10-14" and ((df.condom == "yes") | (df.hts == "yes") | (df.post_violence_care == "yes") | (df.socioeco_app == "yes") | (df.prep == "yes")| (df.contraceptive == "yes"))) else "no"
 
 
 def sec_1519(df):
-    return "secondary" if (df.age_range == "15-19" and ((df.hts == "yes") | (df.post_violence_care == "yes") | (df.socioeco_app == "yes") | (df.prep == "yes"))) else "no"
+    return "secondary" if (df.age_range == "15-19" and ((df.hts == "yes") | (df.post_violence_care == "yes") | (df.socioeco_app == "yes") | (df.prep == "yes") | (df.contraceptive == "yes"))) else "no"
 
 
 def sec_2024(df):
-    return "secondary" if (df.age_range == "20-24" and ((df.hts == "yes") | (df.post_violence_care == "yes") | (df.socioeco_app == "yes") | (df.prep == "yes"))) else "no"
+    return "secondary" if (df.age_range == "20-24" and ((df.hts == "yes") | (df.post_violence_care == "yes") | (df.socioeco_app == "yes") | (df.prep == "yes")| (df.contraceptive == "yes"))) else "no"
 
 
 def comp_1014(df):
-    return "complete" if (df.age_range == "10-14" and df.curriculum == "no" and ((df.condom == "yes") | (df.hts == "yes") | (df.post_violence_care == "yes") | (df.socioeco_app == "yes") | (df.prep == "yes"))) else "no"
+    return "complete" if (df.age_range == "10-14" and df.curriculum == "no" and ((df.condom == "yes") | (df.hts == "yes") | (df.post_violence_care == "yes") | (df.socioeco_app == "yes") | (df.prep == "yes")| (df.contraceptive == "yes"))) else "no"
 
 
 def comp_1519(df):
-    return "complete" if (df.age_range == "15-19" and (((df.curriculum == "yes") & (df.condom == "no")) | ((df.curriculum == "no") & (df.condom == "yes"))) and ((df.hts == "yes") | (df.post_violence_care == "yes") | (df.socioeco_app == "yes") | (df.prep == "yes"))) else "no"
+    return "complete" if (df.age_range == "15-19" and (((df.curriculum == "yes") & (df.condom == "no")) | ((df.curriculum == "no") & (df.condom == "yes"))) and ((df.hts == "yes") | (df.post_violence_care == "yes") | (df.socioeco_app == "yes") | (df.prep == "yes")| (df.contraceptive == "yes"))) else "no"
 
 
 def comp_2024(df):
-    return "complete" if (df.age_range == "20-24" and (((df.curriculum == "yes") & (df.condom == "no")) | ((df.curriculum == "no") & (df.condom == "yes"))) and ((df.hts == "yes") | (df.post_violence_care == "yes") | (df.socioeco_app == "yes") | (df.prep == "yes"))) else "no"
-
+    return "complete" if (df.age_range == "20-24" and (((df.curriculum == "yes") & (df.condom == "no")) | ((df.curriculum == "no") & (df.condom == "yes"))) and ((df.hts == "yes") | (df.post_violence_care == "yes") | (df.socioeco_app == "yes") | (df.prep == "yes")| (df.contraceptive == "yes"))) else "no"
 
 
 
 
 def complete_at_least(df):
-    return "yes" if (df.curriculum=="yes" or df.condom=="yes" or df.hts=="yes" or df.post_violence_care=="yes" or df.socioeco_app=="yes" or df.prep=="yes" or df.parenting=="yes") else "no"
+    return "yes" if (df.curriculum=="yes" or df.condom=="yes" or df.hts=="yes" or df.post_violence_care=="yes" or df.socioeco_app=="yes" or df.prep=="yes" or df.parenting=="yes" or df.contraceptive=='yes') else "no"
 
 
 def isEnrolledQ2(date):
