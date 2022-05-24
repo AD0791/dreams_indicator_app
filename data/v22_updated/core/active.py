f30c132581f7c749f74474c4958f1222253d06e9
AD0791 <alexdisla1991@gmail.com>
2022-05-24 09:59:35 -0400

    solidity

--- data/v22_updated/core/active.py
+++ data/v22_updated/core/active.py
@@ -20,9 +20,9 @@ DBNAME = config('DBCaris')
 
 class Set_date(Enum):
     master_start = "2017-10-01"
-    master_end = "2022-05-17"
+    master_end = "2022-05-24"
     period_start = "2021-10-01"
-    period_end = "2022-05-23"
+    period_end = "2022-05-24"
 
 
 # get the engine to connect and fetch
