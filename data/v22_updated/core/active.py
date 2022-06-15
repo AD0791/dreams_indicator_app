6b13aa2318d4ab02210d9e610adb641fb7801ecc
AD0791 <alexdisla1991@gmail.com>
2022-06-15 10:21:16 -0400

    dollars

--- data/v22_updated/core/active.py
+++ data/v22_updated/core/active.py
@@ -20,9 +20,9 @@ DBNAME = config('DBCaris')
 
 class Set_date(Enum):
     master_start = "2017-10-01"
-    master_end = "2022-06-14"
+    master_end = "2022-06-15"
     period_start = "2021-10-01"
-    period_end = "2022-06-14"
+    period_end = "2022-06-15"
 
 
 # get the engine to connect and fetch
