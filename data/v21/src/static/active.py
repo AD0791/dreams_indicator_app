fbbdfbd7edb20516ce9d9f6d202970d8f6111101
AD0791 <alexdisla1991@gmail.com>
2022-09-29 12:49:44 -0400

    ONFY2020

--- data/v21/src/static/active.py
+++ data/v21/src/static/active.py
@@ -18,9 +18,9 @@ DBNAME = config('DBCaris')
 
 class Set_date(Enum):
     master_start = "2017-10-01"
-    master_end = "2021-03-31"
+    master_end = "2020-11-31"
     period_start = "2020-10-01"
-    period_end = "2021-03-31"
+    period_end = "2020-11-31"
 
 
 # get the engine to connect and fetch
