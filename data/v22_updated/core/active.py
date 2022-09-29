7a21a39b8f0c99d97a2fd2357ae9caf5db77da8d
AD0791 <alexdisla1991@gmail.com>
2022-09-29 12:07:12 -0400

    Mel plan dreams onfy21

--- data/v22_updated/core/active.py
+++ data/v22_updated/core/active.py
@@ -20,14 +20,15 @@ DBNAME = config('DBCaris')
 
 
 class Set_date(Enum):
+    #master_start = "2017-10-01"
+    #master_end = datetime.today().strftime('%Y-%m-%d')
     master_start = "2017-10-01"
-    #master_start = "2018-04-01"
-    #master_end = "2022-06-31"
-    master_end = datetime.today().strftime('%Y-%m-%d')
+    master_end = "2021-11-31"
+    
+    #period_start = "2021-10-01"
+    #period_end = datetime.today().strftime('%Y-%m-%d')
     period_start = "2021-10-01"
-    #period_start = "2022-04-01"
-    #period_end = "2022-06-31"
-    period_end = datetime.today().strftime('%Y-%m-%d')
+    period_end = "2021-11-31"
 
 
 # get the engine to connect and fetch
